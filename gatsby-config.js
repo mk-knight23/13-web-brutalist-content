8a1d0e59fdb55c46a73f27a8d78b16fd937e73b0
mk.knight23 <mk.knight23@example.com>
2026-01-20 23:42:29 +0530

    feat: prepare for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
+    'gatsby-plugin-sass',
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-datocms`,
